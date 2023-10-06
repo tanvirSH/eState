c26834feba3dda7b024ef999a493f737ce47f1a3
Md Taukir <taukirmd2493@gmail.com>
2023-10-07 02:16:16 +0530

    Routes and pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-400 font-bold underline">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-out" element={<SignOut />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
